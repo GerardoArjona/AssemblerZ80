04482bfc088e2b5b19160b6c083d4ab17ab44aa4
Gerardo Arjona <gerardo.arj15@gmail.com>
2018-05-31 11:02:25 -0500

    Se checa que el archivo sea un archivo .asm y su existencia. rocesamiento de directivas y completar las tablas
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

--- PrimeraPasada.java
+++ PrimeraPasada.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PrimeraPasada{
 
@@ -19,44 +21,35 @@ public class PrimeraPasada{
             BufferedReader br = new BufferedReader(new FileReader(fileName));
             BufferedReader ts = new BufferedReader(new FileReader(fileNameSplit[0]+"TS.txt"));
             BufferedReader mnl = new BufferedReader(new FileReader("MnLong.txt"));
+            ArrayList<String> etis = new ArrayList<String>(); //todas las etiquetas que se encuentren en el archivo se guardan aquí
             String line;
             String l;
             String mn;
-            out:
             while(!((line = br.readLine()).equals("    END"))) {
                 if(line.indexOf(":")!=-1){
                     String[] etiDef=line.split(":");
                     String eti=etiDef[0].replace(" ", "");
+                    etis.add(etiDef[0]);
                     while((l=ts.readLine())!=null) {
                         String[] lSplit=l.split("\\|");
-                        System.out.println(etiDef[0]);
-                        if(lSplit[0].equals(etiDef[0])){
-                            System.out.println("Error");
-                            ts.close();  
-                            File archts = new File(fileNameSplit[0]+"TS.txt");
-                            File archcl = new File(fileNameSplit[0]+"CL.txt");
-                            File archcod = new File(fileNameSplit[0]+"COD.txt");                          
-                            archcl.delete();
-                            archts.delete();
-                            archcod.delete();
-                            break out;
-                        }else{
-                            hexCL = Integer.toHexString(cl);
-                            Writer output = new BufferedWriter(new FileWriter(fileNameSplit[0]+"TS.txt", true));  
-                            output.append(eti+"|"+hexCL+"|s\r\n");
-                            output.close();
-                            String[] instruccion=etiDef[1].split(" ");                
-                            while((mn=mnl.readLine())!=null){
-                                String[] ins=mn.split("\\|");
-                                if (ins[0].equals(instruccion[1])){
-                                    lon=Integer.parseInt(ins[1]);
-                                    break;
-                                }            
-                            }
-                            cl=cl+lon;
-                            break;
+                        hexCL = Integer.toHexString(cl);
+                        Writer output = new BufferedWriter(new FileWriter(fileNameSplit[0]+"TS.txt", true));
+                        Writer outCl = new BufferedWriter(new FileWriter(fileNameSplit[0]+"CL.txt",true));  
+                        output.append(eti+"|"+hexCL+"|s\r\n");
+                        output.close();
+                        outCl.append(etiDef[1]+"|"+hexCL+"\r\n");
+                        outCl.close();
+                        String[] instruccion=etiDef[1].split(" ");                
+                        while((mn=mnl.readLine())!=null){
+                            String[] ins=mn.split("\\|");
+                            if (ins[0].equals(instruccion[1])){
+                                lon=Integer.parseInt(ins[1]);
+                                break;
+                            }            
                         }
-                    }
+                        cl=cl+lon;
+                        break;
+                        }
                 }else{
                     String[] instruccion=line.split(" ");
                     while((mn=mnl.readLine())!=null){
@@ -69,8 +62,22 @@ public class PrimeraPasada{
                     cl=cl+lon;
                 }
             }
-            System.out.println(cl);
-		}catch(IOException exp){
+            for (int i = 0; i < etis.size(); i++) {
+                for (int j = i + 1 ; j < etis.size(); j++) {
+                    if (etis.get(i).equals(etis.get(j))) {
+                             System.out.println("Error: Definicion multiple de la etiqueta: "+etis.get(i));
+                             ts.close();
+                               File archts = new File(fileNameSplit[0]+"TS.txt");
+                               File archcl = new File(fileNameSplit[0]+"CL.txt");
+                               File archcod = new File(fileNameSplit[0]+"COD.txt");                          
+                               archcl.delete();
+                               archts.delete();
+                               archcod.delete();
+                               break;
+                    }
+               }
+           }
+        }catch(IOException exp){
 			System.out.println("Error: No se pudo abrir o crear el archivo");
 			exp.printStackTrace();
 		}
--- Traductor.java
+++ Traductor.java
@@ -8,9 +8,10 @@ public class Traductor{
 
         System.out.println("Ingrese el nombre del archivo: ");
         String fileName=sc.nextLine();
+        String[] filesplit=fileName.split("\\.");
         File f=new File(fileName);
 
-        if(f.exists()){
+        if(f.exists() && filesplit[1].contains("asm")){
             CreadorTablas creadorTablas = new CreadorTablas();        
             creadorTablas.creandoTablas(fileName, "TS","SIMBOLO|VALOR|DEFINICION" );
             creadorTablas.creandoTablas(fileName, "CL","ISNTRUCCION|CL" );
@@ -20,7 +21,7 @@ public class Traductor{
             p1.pasadaUno(fileName);
         }
         else
-            System.out.println("\nEl archivo no existe");
+            System.out.println("\nEl archivo no existe o la extension no es valida");
 
 
 
